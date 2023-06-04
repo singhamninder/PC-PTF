deaca49fd83c66e86f12c9997ffcc17539229fa7
Amninder Singh <asing075@ucr.edu>
2023-06-03 17:10:45 -0700

    add utils

--- app.py
+++ app.py
@@ -1,109 +1,9 @@
-import streamlit as st
-import tensorflow as tf
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-import seaborn as sns
-from rosetta import Rosetta
-import pickle
-import random
-
-
-def create_Xtest(data):
-    """Function to generate the data to be used as input to the model"""
-    pF = pd.Series(np.arange(-1, 6, 0.05), name='pF')
-    df = pd.concat([data.drop_duplicates()]*len(pF), ignore_index=True)
-    test = pd.concat([df,pF], axis=1)
-    return test
-
-def wettodry(vwc):
-    vwc = [0 if i < 0 else i for i in vwc]
-    w2d = [vwc[0]]
-    for idx in range(1, len(vwc)):
-        if vwc[idx] < w2d[idx-1]:
-            w2d.append(vwc[idx])
-        else:
-            w2d.append(w2d[idx-1])
-    return w2d
-
-def drytowet(vwc):
-    vwc = [0 if i < 0 else i for i in vwc]
-    rslt_reverse = vwc[::-1]
-    d2w = [rslt_reverse[0]]
-    for idx in range(1, len(vwc)):
-        if rslt_reverse[idx] > d2w[idx-1]:
-            d2w.append(rslt_reverse[idx])
-        else:
-            d2w.append(d2w[idx-1])
-    return d2w[::-1]
-
-@st.cache
-def bag_predict(models, X_test):
-    bag_pred = pd.DataFrame()
-    for ann in models:
-        ann_all = tf.keras.models.load_model(ann)
-        y_pred = ann_all.predict(X_test)
-        w2d = wettodry(y_pred.ravel().tolist())
-        d2w = drytowet(y_pred.ravel().tolist())
-        pred_list = [(g + h) / 2 for g, h in zip(w2d, d2w)]
-        bag_pred[ann[ann.find('ann'):-3]] = pd.Series(pred_list)
-
-    mean_vwc = bag_pred.mean(axis=1)
-    std_vwc = bag_pred.std(axis=1)
-    return mean_vwc, std_vwc
-
-def van_genuchten(h, parms):
-    theta_r = parms[0]
-    theta_s = parms[1]
-    alpha = 10**parms[2]
-    neta = 10**parms[3]
-    m = 1-1/neta
-
-    numt = (theta_s- theta_r)
-    Se=(1+abs(alpha*h)**neta)**(m)
-    vwc = theta_r + numt/Se
-    return vwc
-
-rosetta_vwc_df = pd.DataFrame()
-def plot_results(results_df):
-    rand_soil = random.choice(results_df['soil#'].unique())   
-    sns.set(font_scale=1.5, style="ticks")
-    fig, ax = plt.subplots(figsize=(6, 6))
-    if not rosetta_vwc_df.empty:
-        soil_test = results_df[results_df['soil#']==rand_soil]
-        ax.plot(soil_test['pF'], soil_test['mean_vwc'], '-', label = 'PCPTF')
-        ax.fill_between(soil_test['pF'], soil_test['mean_vwc']+soil_test['std_vwc'],
-                        soil_test['mean_vwc']-soil_test['std_vwc'], alpha=0.3)
-        rosetta_soil = rosetta_vwc_df[rosetta_vwc_df['soil#']==rand_soil]
-        ax.plot(rosetta_soil['pF'], rosetta_soil['rosetta'], '-', label = 'Rosetta3')
-        ax.fill_between(rosetta_soil['pF'], rosetta_soil['std'], rosetta_soil['_std'], alpha=0.3)
-    else:
-        soil_test = results_df[results_df['soil#']==rand_soil]
-        ax.plot(soil_test['pF'], soil_test['mean_vwc'], '-', label = 'PCPTF')
-        ax.fill_between(soil_test['pF'], soil_test['mean_vwc']+soil_test['std_vwc'],
-                        soil_test['mean_vwc']-soil_test['std_vwc'], alpha=0.3)
-
-    ax.set_xlim(0,5)
-    ax.legend(prop={'size': 14})
-    ax.grid(True,linestyle='--')
-    # ax.vlines(x = 4.2, ymin = 0, ymax = 0.6,
-    #        colors = 'tab:red',
-    #        label = 'PWP')
-    ax.xaxis.set_major_locator(MultipleLocator(1))
-    ax.yaxis.set_major_locator(MultipleLocator(.1))
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.set_xlabel('pF',size = 18, weight = 'bold')
-    ax.set_ylabel('VWC'r' [$cm^3 cm^{-3}$]',size = 18, weight = 'bold')
-    ax.set_title("soil# = "+ str(rand_soil))
-    return fig
-
+from utils import *
 
 st.title('Pseudo-continuous Pedotransfer Functions for estimating Soil Water Retention Curve (SWRC)')
 with st.sidebar:
     st.subheader('Upload your CSV file')
-    uploaded_file = st.file_uploader("Make sure columns are named - soil#, clay, silt, sand, bd, and omc", type=["csv"],
+    uploaded_file = st.file_uploader("Make sure columns are named - soil#, clay, silt, sand, BD, and omc", type=["csv"],
                 help='File should atleast have columns - soil#, clay, silt, and sand')
 
     st.markdown('Clay [%],  Silt [%], Sand [%], ' 
@@ -115,12 +15,12 @@ if uploaded_file is not None:
     st.markdown('**Glimpse of your dataset**')
     st.write(df)
 
-    if set(['clay', 'silt', 'sand', 'bd', 'omc']).issubset(df.columns):
+    if set(['clay', 'silt', 'sand', 'BD', 'omc']).issubset(df.columns):
         st.success('All required columns are present')
         choices = (['model1', 'model2', 'model3', 'model4'])
 
-    elif set(['clay', 'silt', 'sand', 'bd']).issubset(df.columns):
-        st.success('clay, silt, sand, and bulk density(bd) columns are present')
+    elif set(['clay', 'silt', 'sand', 'BD']).issubset(df.columns):
+        st.success('clay, silt, sand, and bulk density(BD) columns are present')
         choices = (['model2', 'model3'])
     
     elif set(['clay', 'silt', 'sand', 'omc']).issubset(df.columns):
@@ -140,10 +40,10 @@ if uploaded_file is not None:
     model = st. selectbox('Select model', choices)
 
     if model == 'model1':
-        st.info(f'{model} uses soil texture(SSC), bulk density(bd), and soil organic matter (omc) as inputs')
+        st.info(f'{model} uses soil texture(SSC), bulk density(BD), and soil organic matter (omc) as inputs')
         models = ['model1/ann_'+ str(i) + '.h5' for i in range(100)]
         ## generate the test dataset for the model
-        colList = ['soil#', 'clay', 'silt', 'sand', 'bd', 'omc']
+        colList = ['soil#', 'clay', 'silt', 'sand', 'BD', 'omc']
         df_group = df[colList].groupby(['soil#'])
         test_df = df_group.apply(create_Xtest).reset_index(drop=True)
         #load scaler and fit the models
@@ -218,10 +118,10 @@ if uploaded_file is not None:
         st.write(vg_parms_df)
 
     elif model == 'model3':
-        st.info(f'{model} uses soil texture(SSC), and bulk density(bd) as inputs')
+        st.info(f'{model} uses soil texture(SSC), and bulk density(BD) as inputs')
         models = ['model3/ann_'+ str(i) + '.h5' for i in range(100)]
         ## generate the test dataset for the model
-        colList = ['soil#', 'clay', 'silt', 'sand', 'bd']
+        colList = ['soil#', 'clay', 'silt', 'sand', 'BD']
         df_group = df[colList].groupby(['soil#'])
         test_df = df_group.apply(create_Xtest).reset_index(drop=True)
         #load scaler and fit the models
@@ -240,7 +140,7 @@ if uploaded_file is not None:
             results_df = pd.concat([test_df, vwc_.iloc[:,1:]],axis=1)
 
             ##Get van genuchten parameters from Rosetta3 SWRC
-            rosetta_input = np.array(soil_test[['sand','silt','clay', 'bd']].head(1))
+            rosetta_input = np.array(soil_test[['sand','silt','clay', 'BD']].head(1))
             rose33 = Rosetta(rosetta_version=3, model_code=3)
             mean, stdev = rose33.predict(rosetta_input)
             vg_parms[soil] = pd.DataFrame(mean, columns=['theta_r', 'theta_s',
